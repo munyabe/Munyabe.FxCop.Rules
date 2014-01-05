d4953b3d79f6b694306819045e660b2f7ef01160
munyabe <yo.betek@gmail.com>
2014-01-05 15:03:34 +0900

    Add AvoidEnumGetHashCode rule.

---
+++ CodeAnalysisTestTarget/Performance/AvoidEnumGetHashCodeTarget.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeAnalysisTestTarget.Performance
+{
+    public static class AvoidEnumGetHashCodeTarget
+    {
+        public static void OK()
+        {
+            var code = ((int)Hoge.A).GetHashCode();
+        }
+
+        public static void NG()
+        {
+            var code = Hoge.A.GetHashCode();
+        }
+
+        public enum Hoge
+        {
+            A,
+            B,
+            C
+        }
+    }
+}
---
+++ Munyabe.FxCop.Rules/Performance/AvoidEnumGetHashCode.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.FxCop.Sdk;
+
+namespace Munyabe.FxCop.Performance
+{
+    /// <summary>
+    /// <see cref="Enum.GetHashCode"/>の呼び出しを検出する解析ルールです。
+    /// </summary>
+    public class AvoidEnumGetHashCode : BaseRule
+    {
+        private static Method _getHashCodeMethod;
+
+        /// <summary>
+        /// インスタンスを初期化します。
+        /// </summary>
+        public AvoidEnumGetHashCode()
+            : base("AvoidEnumGetHashCode")
+        {
+        }
+
+        /// <inheritdoc />
+        public override void BeforeAnalysis()
+        {
+            _getHashCodeMethod = FrameworkTypes.Object.GetMethod(Identifier.For("GetHashCode"));
+        }
+
+        /// <inheritdoc />
+        public override void AfterAnalysis()
+        {
+            _getHashCodeMethod = null;
+        }
+
+        /// <inheritdoc />
+        public override ProblemCollection Check(Member member)
+        {
+            if (member is Method)
+            {
+                Visit(member);
+            }
+
+            return Problems;
+        }
+
+        /// <inheritdoc />
+        public override void VisitMethodCall(MethodCall call)
+        {
+            if (IsEnumGetHashCodeCall(call))
+            {
+                Problems.Add(new Problem(GetResolution()));
+            }
+        }
+
+        /// <summary>
+        /// <see cref="Enum.GetHashCode"/>の呼び出しかどうかを判定します。
+        /// </summary>
+        private static bool IsEnumGetHashCodeCall(MethodCall call)
+        {
+            var member = call.Callee as MemberBinding;
+            if (member == null)
+            {
+                return false;
+            }
+
+            if (member.TargetObject == null || member.TargetObject.Type != FrameworkTypes.Enum)
+            {
+                return false;
+            }
+
+            var method = member.BoundMember as Method;
+            if (method == null)
+            {
+                return false;
+            }
+
+            return method == _getHashCodeMethod;
+        }
+    }
+}
